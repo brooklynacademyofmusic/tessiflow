793e94bf3d552fe3030731f86e4e4d118598590c
skysyzygy <solson@bam.org>
2022-07-19 19:17:16 -0400

    File logging for each job with prepended info and rotation

---
+++ R/job_log.R
@@ -0,0 +1,27 @@
+#' job_log_write
+#'
+#' Simple logfile writer, with filename determined by the workflow. Timestamp and job name are prepended to each line before writing.
+#'
+#' @param flow_name character workflow name
+#' @param job_name character job name
+#' @param lines character vector of lines to write to the file
+#' @importFrom checkmate assert_character
+#' @importFrom lubridate today
+#'
+job_log_write <- function(flow_name, job_name, lines) {
+  assert_character(flow_name, len = 1)
+  assert_character(job_name, len = 1)
+
+  # add time and job info
+  lines <- paste("[", Sys.time(), ":", job_name, "]", lines)
+  filename <- file.path(config::get("tessiflow.log"), paste0(flow_name, ".log"))
+
+  # rotate logs if over 1M
+  if (file.exists(filename) && file.info(filename)$size > 1024^2) {
+    zip_filename <- paste0(gsub(".log", "", filename, fixed = TRUE), "-", today(), ".zip")
+    zip(zip_filename, filename, flags = "-j -q") # -j : "junk" the directory structure
+    if (file.exists(zip_filename)) file.remove(filename)
+  }
+
+  write(lines, filename, sep = "\r\n", append = TRUE)
+}
