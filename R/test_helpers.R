cc44f29eb54a3ac7d72f38f735ef1b7284321d21
skysyzygy <solson@bam.org>
2022-07-19 19:18:02 -0400

    New test helpers

--- R/test_helpers.R
+++ R/test_helpers.R
@@ -1,21 +1,38 @@
 make_fixtures <- function() {
-  
-  files <- dir(testthat::test_path("tessiflow.d"), pattern="*.yml", full.names = TRUE, recursive = TRUE)
+  files <- dir(testthat::test_path("tessiflow.d"), pattern = "*.yml", full.names = TRUE, recursive = TRUE)
   flows <- lapply(files, yaml::read_yaml)
-  saveRDS(flows,testthat::test_path("flows.Rds"))
-  
+  saveRDS(flows, testthat::test_path("flows.Rds"))
+
   flows_data_table <- flows_parse(testthat::test_path("tessiflow.d"))
-  saveRDS(flows_data_table,testthat::test_path("flows_data_table.Rds"))
-  
-  jobs <- with(flows_data_table,data.table(flow_name,job_name,status="Waiting",retval=NA_integer_,start_time=now(),end_time=NA_real_))
-  saveRDS(jobs,testthat::test_path("jobs.Rds"))
+  saveRDS(flows_data_table, testthat::test_path("flows_data_table.Rds"))
+
+  jobs <- with(flows_data_table, data.table(flow_name, job_name, status = "Waiting", retval = NA_integer_, start_time = now(), end_time = NA_real_))
+  saveRDS(jobs, testthat::test_path("jobs.Rds"))
 }
 
-local_log_dir <- function(envir=parent.frame()) {
-  dir.create(file.path(tempdir(),"logs"))
-  withr::defer({
-    flows_log_close()
-    gc()
-    unlink(file.path(tempdir(),"logs"),recursive = TRUE, force = TRUE)
-  },envir=envir)
+local_log_dir <- function(envir = parent.frame()) {
+  dir.create(file.path(tempdir(), "logs"))
+  withr::defer(
+    {
+      flows_log_close()
+      gc()
+      unlink(file.path(tempdir(), "logs"), recursive = TRUE, force = TRUE)
+    },
+    envir = envir
+  )
+}
+
+local_flows_data_table <- function(envir = parent.frame()) {
+  tessiflow$flows <- readRDS(test_path("flows_data_table.Rds"))
+  tessiflow$flows[1, scheduled_runs := list(list(NULL))]
+  tessiflow$flows[1, `if` := list(list(NULL))]
+  tessiflow$flows[1, `runs-on` := list(list(NULL))]
+  tessiflow$flows[1, needs := list(list(NULL))]
+
+  withr::defer(
+    {
+      tessiflow$flows <- NULL
+    },
+    envir = envir
+  )
 }
