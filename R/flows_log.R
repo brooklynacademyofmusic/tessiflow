70c2aafc2991f65d958daf1c5a611752eb244d03
skysyzygy <solson@bam.org>
2022-07-19 19:15:39 -0400

    More columns in flows log, and "upsert" functionality instead of write, so that we can easily update it to match the local flows data.table

--- R/flows_log.R
+++ R/flows_log.R
@@ -5,21 +5,18 @@
 #' @return invisible
 #'
 flows_log_open <- function(flows_log_dir = config::get("tessiflow.log")) {
-  if (is.null(flows_log_dir) || !dir.exists(flows_log_dir))
+  if (is.null(flows_log_dir) || !dir.exists(flows_log_dir)) {
     stop("Please set the tessiflow.log config option to a directory where log files will be stored")
-  
-  if (is.null(tessiflow$db)) {
-      tessiflow$db <- DBI::dbConnect(RSQLite::SQLite(), file.path(flows_log_dir,"flows.sqlite"))
   }
-  
-  if (!DBI::dbExistsTable(tessiflow$db,"jobs"))
-    DBI::dbCreateTable(tessiflow$db,"jobs",fields = c(flow_name = "character",
-                                                      job_name = "character",
-                                                      status = "character",
-                                                      start_time = "double",
-                                                      end_time = "double",
-                                                      retval = "integer"))
-  
+
+  if (is.null(tessiflow$db)) {
+    tessiflow$db <- DBI::dbConnect(RSQLite::SQLite(), file.path(flows_log_dir, "flows.sqlite"))
+  }
+
+  if (!DBI::dbExistsTable(tessiflow$db, "jobs")) {
+    flows_log_create()
+  }
+
   invisible()
 }
 
@@ -27,29 +24,65 @@ tessiflow <- new.env()
 
 #' @describeIn flows_log_open Tear down the SQL connection
 flows_log_close <- function() {
-  if(!is.null(tessiflow$db))
+  if (!is.null(tessiflow$db)) {
     DBI::dbDisconnect(tessiflow$db)
+  }
   tessiflow$db <- NULL
 }
 
-db <- new.env(parent = emptyenv())
+#' @describeIn flows_log_open Create the SQLite log table
+flows_log_create <- function() {
+  DBI::dbCreateTable(tessiflow$db, "jobs", fields = c(
+    flow_name = "character",
+    job_name = "character",
+    `runs-on` = "character",
+    steps = "character",
+    needs = "character",
+    `if` = "character",
+    status = "character",
+    start_time = "double",
+    end_time = "double",
+    retval = "integer"
+  ))
 
-#' flows_log_write
+  DBI::dbExecute(tessiflow$db, "CREATE UNIQUE INDEX jobs_index ON jobs(flow_name,job_name,start_time)")
+}
+
+#' flows_log_upsert
 #'
 #' @param table string, right now only `jobs` is valid
-#' @param data data.frame of data to write
-#'
+#' @param data data.frame of data to update, rows will be matched on flow_name, job_name, and start_time
+#' @importFrom dplyr copy_to
+#' @importFrom checkmate assert_names assert_data_frame
 #' @return invisibly
-#' @importFrom checkmate assert_choice assert_names assert_class
-#'
-flows_log_write <- function(table = "jobs",data) {
-  assert_choice(table,"jobs")
+flows_log_upsert <- function(table = "jobs", data) {
+  assert_choice(table, "jobs")
   flows_log_open()
-  assert_names(names(data),subset.of=DBI::dbListFields(tessiflow$db,"jobs"),
-               .var.name = names(rlang::enexpr(data)))
-  assert_class(data,"data.frame")
-  
-  DBI::dbAppendTable(tessiflow$db,table,data)
+  matching_cols <- c("job_name", "flow_name", "start_time")
+  assert_names(names(data),
+    # subset.of=DBI::dbListFields(tessiflow$db,"jobs"),
+    must.include = matching_cols,
+    .var.name = names(rlang::enexpr(data))
+  )
+  assert_data_frame(data, min.rows = 1)
+
+  data <- dplyr::select(data, intersect(names(data), DBI::dbListFields(tessiflow$db, "jobs"))) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate_if(~ !is.double(.) && !is.integer(.), as.character)
+
+  copy_to(tessiflow$db, data, "flows_log_upsert", overwrite = TRUE, temporary = TRUE)
+
+  data_names <- paste0("\"", names(data), "\"")
+
+  sql <- paste(
+    "INSERT INTO", table, "(", paste(data_names, collapse = ", "), ")",
+    "SELECT", paste(data_names, collapse = ", "),
+    "FROM flows_log_upsert f WHERE true",
+    "ON CONFLICT (", paste(matching_cols, collapse = ", "), ")",
+    "DO UPDATE SET", paste(paste0(data_names, " = excluded.", data_names), collapse = ", ")
+  )
+
+  DBI::dbExecute(tessiflow$db, sql)
 }
 
 #' flows_log_get_last_run
@@ -61,19 +94,20 @@ flows_log_write <- function(table = "jobs",data) {
 #' @importFrom checkmate assert_character
 #' @importFrom dplyr tbl inner_join group_by slice_max collect
 #'
-flows_log_get_last_run <- function(flows,jobs) {
+flows_log_get_last_run <- function(flows, jobs) {
   flow_name <- job_name <- start_time <- NULL
-  
-  assert_character(flows,any.missing = FALSE)
-  assert_character(jobs,any.missing = FALSE)
+
+  assert_character(flows, any.missing = FALSE)
+  assert_character(jobs, any.missing = FALSE)
   flows_log_open()
-  
-  query_tbl = data.frame(flow_name = flows,job_name = jobs)
-  
-  tbl(tessiflow$db,"jobs") %>% 
-    inner_join(query_tbl,by=c("flow_name","job_name"),copy = TRUE) %>%
-    group_by(flow_name,job_name) %>%
-    slice_max(start_time,1) %>% collect
+
+  query_tbl <- data.frame(flow_name = flows, job_name = jobs)
+
+  tbl(tessiflow$db, "jobs") %>%
+    inner_join(query_tbl, by = c("flow_name", "job_name"), copy = TRUE) %>%
+    group_by(flow_name, job_name) %>%
+    slice_max(start_time, 1) %>%
+    collect()
 }
 
 #' flows_log_get_create_time
@@ -82,6 +116,6 @@ flows_log_get_last_run <- function(flows,jobs) {
 #'
 flows_log_get_create_time <- function() {
   flows_log_open()
-  
+
   file.info(DBI::dbGetInfo(tessiflow$db)$dbname)$ctime
 }
