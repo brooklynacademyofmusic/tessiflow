c1d59149932703dc81b41656706ddd15f0bab115
skysyzygy <solson@bam.org>
2022-08-08 17:02:25 -0400

    Work towards a tessiflow daemon - tessiflow_run wraps the main loop with some logging and safe starting - tessiflow_stop kills all running instances - tessiflow_enable and disable are convenience functions that create scheduled jobs

--- R/tessiflow.R
+++ R/tessiflow.R
@@ -1,11 +1,11 @@
 
-#' tessiflow_start
+#' tessiflow_run
 #'
-#' Start a background tessiflow process
-#'
-#' @return the process object
+#' Wraps flows_main with some logging and environment variables
+#' @importFrom withr local_envvar local_output_sink local_message_sink
+#' @return nothing, invisibly
 #' @export
-tessiflow_start <- function() {
+tessiflow_run <- function() {
   flows_log_dir = config::get("tessiflow.log")
   if (is.null(flows_log_dir) || !dir.exists(flows_log_dir)) {
     stop("Please set the tessiflow.log config option to a directory where log files will be stored")
@@ -14,62 +14,84 @@ tessiflow_start <- function() {
   # called for error-checking side-effects
   flows_parse()
   
-  tree <- ps::ps_find_tree("tessiflow_daemon")
+  tree <- ps::ps_find_tree("tessiflow-daemon")
   if(length(tree)>0) {
-    stop("Running tessiflow process found, cowardly refusing to start another.")
+    stop("Found running tessiflow process, cowardly refusing to start another.")
   } 
   
   logfile <- file.path(flows_log_dir, "tessiflow.log")
+  log_rotate(logfile)
   
- # rotate old logfile
-  if (file.exists(logfile)) {
-    zip_filename <- paste0(gsub(".log", "", logfile, fixed = TRUE), "-", as.Date(file.info(logfile)$ctime), ".zip")
-    zip(zip_filename, logfile, flags = "-j -q") # -j : "junk" the directory structure
-    if (file.exists(zip_filename)) file.remove(logfile)
-  }
-
-  tessiflow$r <- callr::r_bg(eval,
-    supervise = FALSE,
-    args = list(quote({
-      tessiflow::flows_main()
-    })),
-    env = c("tessiflow_daemon"="YES"),
-    stdout = logfile,
-    stderr = "2>&1",
-    poll_connection = FALSE
-  )
+  local_envvar("tessiflow-daemon"="YES")
+  local_output_sink(logfile, append = TRUE, split = TRUE, .local_envir = environment())
+  local_message_sink(logfile, append = TRUE, .local_envir = environment())
+  
+  cat(paste("[", Sys.time(), ": tessiflow ]", "Starting tessiflow scheduler ...\n"))
+  
+  flows_main()
+  
+  invisible()
 }
 
 #' tessiflow_stop
 #'
 #' @importFrom ps ps_kill_tree
 tessiflow_stop <- function() {
-  if (!is.null(tessiflow$r)) {
-    tessiflow$r$kill()
-    tessiflow$r <- NULL
-  } else {
-    ps::ps_kill_tree("tessiflow_daemon")
-  }
+  ps_kill_tree("tessiflow-daemon")
 }
 
+#' tessiflow_enable
+#'
+#' Schedule a keep-alive job that runs tessiflow_run.
+#'
+#' @return invisibly
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' tessiflow::tessiflow_enable()
+#' }
 tessiflow_enable <- function() {
-  system(paste(
-    "schtasks", "/create",
-    "/tn", "tessiflow",
-    "/sc", "minute",
-    "/mo", 1,
-    "/tr", shQuote(paste(
-      Sys.which("Rscript"),
-      "-e", "tessiflow::tessiflow_start()",
-      "--vanilla"
-    ))
-  ))
+  schedule <- if(.Platform$OS.type == "windows") {
+    schedule_schtasks
+  } else {
+    schedule_crontab
+  }
+
+  schedule(rlang::expr(tessiflow::tessiflow_run()),"tessiflow")
+  
+  invisible()
 }
 
+#' @describeIn tessiflow_enable Unschedule the tessiflow job that runs tessiflow_run.
+#' @export
 tessiflow_disable <- function() {
-  system(paste("schtasks", "/delete", "/tn", "tessiflow"))
+  unschedule <- if(.Platform$OS.type == "windows") {
+    unschedule_schtasks
+  } else {
+    unschedule_crontab
+  }
+  
+  unschedule("tessiflow")
+  
+  invisible()
 }
 
+#' tessiflow_job_start
+#' 
+#' Starts a tessiflow job identified by `flow_name` and `job_name`
+#'
+#' @param flow_name string flow name
+#' @param job_name sting job name
+#'
+#' @return invisibly
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' tessiflow::tessiflow_job_start("Workflow A","Job 1")
+#' tessiflow::tessiflow_job_stop("Workflow A","Job 1")
+#' }
 tessiflow_job_start <- function(flow_name, job_name) {
   assert_flow_job_name(flow_name, job_name)
   
@@ -81,5 +103,25 @@ tessiflow_job_start <- function(flow_name, job_name) {
   conns <- rbindlist(lapply(tree,ps::ps_connections))
   socket <- socketConnection(port = conns$lport[[1]])
   
-  writeLines(deparse(rlang::expr(job_start(!!flow_name, !!job_name))))
+  writeLines(deparse(rlang::expr(job_start(!!flow_name, !!job_name))),socket)
+  
+  invisible()
+}
+
+#' @describeIn tessiflow_job_start Stops a tessiflow job identified by `flow_name` and `job_name`
+#' @export
+tessiflow_job_start <- function(flow_name, job_name) {
+  assert_flow_job_name(flow_name, job_name)
+  
+  tree <- ps::ps_find_tree("tessiflow")
+  if(length(tree)==0) {
+    stop("No running tessiflow process found, can't start job")
+  } 
+  
+  conns <- rbindlist(lapply(tree,ps::ps_connections))
+  socket <- socketConnection(port = conns$lport[[1]])
+  
+  writeLines(deparse(rlang::expr(job_start(!!flow_name, !!job_name))),socket)
+  
+  invisible()
 }
--- tests/testthat/test-tessiflow.R
+++ tests/testthat/test-tessiflow.R
@@ -1,44 +1,134 @@
-local_log_dir()
+withr::local_package("mockery")
+withr::local_package("devtools")
 
-# tessiflow_start ---------------------------------------------------------
-
-test_that("tessiflow_start starts a background process",{
-  expect_equal(length(ps::ps_find_tree("tessiflow_daemon")),0)
-  tessiflow_start()
-  expect_gte(length(ps::ps_find_tree("tessiflow_daemon")),1)
+# tessiflow_run ---------------------------------------------------------
+run_expr <- quote({
+  tessiflow:::local_log_dir(envir=new.env())
+   tryCatch(load_all(quiet = TRUE),
+            error = function(e) {library("tessiflow")})
+  mockery::stub(tessiflow_run,"flows_main",function() {
+    message("Running flows_main()")
+    Sys.sleep(1) # just here to separate these messages from leaking into the logdir cat 
+    cat(config::get("tessiflow.log"),sep="\n")
+    Sys.sleep(10) # has to be long enough to allow the process to persist between tests
+  })
+  tessiflow_run()
 })
 
-test_that("tessiflow_start refuses to start if one is already running",{
-  expect_error(tessiflow_start(),"Running tessiflow")
-  expect_gte(length(ps::ps_find_tree("tessiflow_daemon")),1)
+num_processes <- 0
+
+test_that("tessiflow_run refuses to start if tessiflow is already running",{
+  expect_equal(length(ps::ps_find_tree("tessiflow-daemon")),0)
+  
+  p1 <- callr::r_bg(eval,list(run_expr))
+  p1$poll_io(10000)
+  p1_output <- p1$read_output_lines()
+  expect_match(p1_output,"Starting tessiflow",all=FALSE)
+  num_processes <<- length(ps::ps_find_tree("tessiflow-daemon"))
+  expect_gte(num_processes,1)
+  
+  p2 <- callr::r_bg(eval,list(run_expr))
+  p2$poll_io(10000)
+  p2_error <- p2$read_error_lines()
+  expect_match(p2_error,"Found running tessiflow",all=FALSE)
+  expect_equal(length(ps::ps_find_tree("tessiflow-daemon")),num_processes)
+  
+  p1$kill_tree()
+  p2$kill_tree()
 })
 
-test_that("tessiflow_start logs to a log file",{
- Sys.sleep(2)
- expect_gte(length(readLines(file.path(config::get("tessiflow.log"),"tessiflow.log"))),1)
+test_that("tessiflow_run logs to a log file",{
+  expect_equal(length(ps::ps_find_tree("tessiflow-daemon")),0)
+  
+  p1 <- callr::r_bg(eval,list(run_expr))
+  p1$poll_io(10000)
+  p1_output <- p1$read_output_lines()
+  expect_match(p1_output,"Starting tessiflow")
+  
+  Sys.sleep(1)
+  while(length(logdir <- p1$read_output_lines())==0)
+    p1$poll_io(10000)
+  
+  expect_length(logdir,1) # message isn't printed to console
+  logdata <- readLines(file.path(logdir,"tessiflow.log"))
+  expect_match(logdata,"Starting tessiflow",all = FALSE)
+  expect_match(logdata,"Running flows_main",all = FALSE)
+  expect_equal(length(logdata),3) 
+  
+  p1$kill_tree()
 })
 
 # tessiflow_stop ----------------------------------------------------------
 
 test_that("tessiflow_stop kills the daemon process",{
- expect_gte(length(ps::ps_find_tree("tessiflow_daemon")),1)
+ p1 <- callr::r_bg(eval,list(run_expr))
+ p1$poll_io(10000)
+ expect_gte(length(ps::ps_find_tree("tessiflow-daemon")),1)
  tessiflow_stop()
- expect_equal(length(ps::ps_find_tree("tessiflow_daemon")),0)
+ expect_equal(length(ps::ps_find_tree("tessiflow-daemon")),0)
 })
 
-test_that("tessiflow_stop kills all running jobs",{})
+test_that("tessiflow_stop kills all running jobs",{
+  run_expr <- quote({
+    tessiflow:::local_log_dir(envir=new.env())
+    tryCatch(load_all(quiet = TRUE),
+             error = function(e) {library("tessiflow")})
+    mockery::stub(tessiflow_run,"flows_main",function() {
+      callr::r(Sys.sleep,list(10))
+    })
+    tessiflow_run()
+  })
+  
+  p1 <- callr::r_bg(eval,list(run_expr))
+  p1$poll_io(10000)
+  Sys.sleep(1)
+  expect_gt(length(ps::ps_find_tree("tessiflow-daemon")),num_processes)
+  
+  tessiflow_stop()
+  expect_equal(length(ps::ps_find_tree("tessiflow-daemon")),0)
+})
 
 # tessiflow_enable --------------------------------------------------------
 
-test_that("tessiflow_enable adds tessiflow to schtasks on Windows",{})
-
-test_that("tessiflow_enable adds tessiflow to cron on *nix",{})
+test_that("tessiflow_enable schedules tessiflow",{
+  schedule_schtasks <- mock()
+  schedule_crontab <- mock()
+  Platform <- copy(.Platform)
+  stub(tessiflow_enable,"schedule_schtasks",schedule_schtasks)
+  stub(tessiflow_enable,"schedule_crontab",schedule_crontab)  
+  
+  Platform$OS.type <- "windows"
+  assign(".Platform",Platform,envir=environment(tessiflow_enable))
+  tessiflow_enable()
+  
+  Platform$OS.type <- "linux"
+  assign(".Platform",Platform,envir=environment(tessiflow_enable))
+  tessiflow_enable()
+  
+  expect_equal(mock_args(schedule_schtasks)[[1]],list(quote(tessiflow::tessiflow_run()),"tessiflow"))
+  expect_equal(mock_args(schedule_crontab)[[1]],list(quote(tessiflow::tessiflow_run()),"tessiflow"))
+})
 
 # tessiflow_disable -------------------------------------------------------
 
-test_that("tessiflow_enable removes tessiflow from schtasks on Windows",{})
-
-test_that("tessiflow_enable removes tessiflow from cron on *nix",{})
+test_that("tessiflow_disable unschedules tessiflow",{
+  unschedule_schtasks <- mock()
+  unschedule_crontab <- mock()
+  Platform <- copy(.Platform)
+  stub(tessiflow_disable,"unschedule_schtasks",unschedule_schtasks)
+  stub(tessiflow_disable,"unschedule_crontab",unschedule_crontab)  
+  
+  Platform$OS.type <- "windows"
+  assign(".Platform",Platform,envir=environment(tessiflow_disable))
+  tessiflow_disable()
+  
+  Platform$OS.type <- "linux"
+  assign(".Platform",Platform,envir=environment(tessiflow_disable))
+  tessiflow_disable()
+  
+  expect_equal(mock_args(unschedule_schtasks)[[1]],list("tessiflow"))
+  expect_equal(mock_args(unschedule_crontab)[[1]],list("tessiflow"))
+})
 
 # tessiflow_job_start -----------------------------------------------------
 
