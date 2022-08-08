fe7c99807c856e9863fcf5e0a06d7db28369552d
skysyzygy <solson@bam.org>
2022-08-08 17:00:28 -0400

    Add some simple schtasks and cron scheduling

---
+++ R/schtasks_cron.R
@@ -0,0 +1,68 @@
+#' schedule_schtasks
+#' 
+#' creates a job named `taskname` that runs every minute using Windows `schtasks.exe`
+#'
+#' @param expr expression to call in an Rscript process
+#' @param taskname string name of task
+#'
+#' @return result of system call
+schedule_schtasks <- function(expr,taskname="tessiflow") {
+  system2(Sys.which("schtasks"),c(
+                "/create",
+                "/tn", taskname,
+                "/sc", "minute",
+                "/mo", 1,
+                "/tr", shQuote(script_expr(expr))),stdout = TRUE)
+}
+
+#' @describeIn schedule_schtasks deletes a job with the name `taskname` using Windows `schtasks.exe`
+unschedule_schtasks <- function(taskname="tessiflow") {
+  system2(Sys.which("schtasks"),c(
+               "/delete","/f",
+               "/tn", taskname),stdout = TRUE)
+}
+
+#' schedule_crontab
+#'
+#' creates a job named `taskname` that runs every minute using *nix `crontab`
+#'
+#' @param expr expression to call in an Rscript process
+#' @param taskname string name of task
+#' 
+#' @return result of system call
+schedule_crontab <- function(expr,taskname="tessiflow") {
+  crontab_temp <- tempfile()
+  crontab_updated <- c(
+    system2("crontab","-l"),
+    paste("#",taskname),
+    paste("* * * * *     ",script_expr(expr)))
+  writeLines(crontab_updated,crontab_temp)
+  system2(Sys.which("crontab"),crontab_temp)
+}
+
+#' @describeIn schedule_schtasks deletes a job named `taskname` using *nix `crontab`
+unschedule_crontab <- function(taskname="tessiflow") {
+  crontab_temp <- tempfile()
+  crontab_current <- system2("crontab","-l")
+  rows <- grep(paste("#",taskname),crontab_current)
+  writeLines(crontab_current[-c(rows,rows+1)],crontab_temp)
+  system2(Sys.which("crontab"),crontab_temp)
+}
+
+#' script_expr
+#'
+#' @param expr to call in an Rscript process
+#'
+#' @return Rscript command line call
+script_expr <- function(expr) {
+  expr <- rlang::enexpr(expr)
+  if(!is.call(expr)) expr <- eval(expr,envir = parent.frame())
+  
+  r_command <- paste(deparse(expr,width.cutoff = 500),collapse=";")
+  
+  paste(
+    Sys.which("Rscript"),
+    "-e", shQuote(r_command),
+    "--no-save","--no-restore"
+  )  
+}
---
+++ tests/testthat/test-schtasks_cron.R
@@ -0,0 +1,67 @@
+withr::local_package("mockery")
+withr::defer(eval(rlang::expr(.Platform$OS.type <- !!.Platform$OS.type)))
+.Platform$OS.type <- "windows"
+
+temp <- tempfile()
+expr <- rlang::expr({print("Here I am!"); writeLines("hello world!",!!temp)})
+
+if(.Platform$OS.type != "windows") {
+  stub(schedule_schtasks,"system2",function(...) {"SUCCESS"})
+  stub(unschedule_schtasks,"system2",function(...) {"SUCCESS"})
+}
+
+test_that("script_expr creates a runnable string", {
+  withr::local_file(temp)
+  expect_match(system(script_expr(expr),intern = TRUE),"Here I am!")
+  expect_equal(readLines(temp),"hello world!")
+})
+
+test_that("schedule_schtasks runs successfully", {
+  return <- schedule_schtasks(expr,"dummy_task")
+  expect_match(return,"SUCCESS")
+})
+
+test_that("schedule_schtasks creates a usable task", {
+  withr::local_file(temp)
+  if(.Platform$OS.type == "windows") {
+    expect_match(system("schtasks /run /tn dummy_task",intern=TRUE),"SUCCESS")
+  } else {
+    eval(expr)
+  }
+  expect_equal(readLines(temp),"hello world!")
+})
+
+test_that("schedule_schtasks deletes the task", {
+  return <- unschedule_schtasks("dummy_task")
+  expect_match(return,"SUCCESS")
+})
+
+filename <- NULL
+
+test_that("schedule_crontab adds the script to the crontab file", {
+  system2 <- mock("this is a task already there",0)
+  stub(schedule_crontab,"system2",system2)
+  schedule_crontab(expr,"dummy_task")
+  filename <<- mock_args(system2)[[2]][[2]]
+  crontab <- readLines(filename)
+  expect_length(crontab,3)
+  expect_match(crontab,"# dummy_task",all = FALSE)
+  expect_match(crontab,"hello world!",all = FALSE)
+})
+
+test_that("schedule_crontab creates a usable task", {
+  withr::local_file(temp)
+  crontab <- readLines(filename)
+  script <- gsub("* * * * *","",tail(crontab,1),fixed=TRUE)
+  system(script,ignore.stdout = TRUE)
+  expect_equal(readLines(temp),"hello world!")
+})
+
+test_that("unschedule_crontab removes the task from crontab", {
+  system2 <- mock(readLines(filename),0)
+  stub(unschedule_crontab,"system2",system2)
+  unschedule_crontab("dummy_task")
+  filename <- mock_args(system2)[[2]][[2]]
+  crontab <- readLines(filename)
+  expect_length(crontab,1)
+})
