0d09f51212dd76bb83a4b50fd9c03ee94694f571
skysyzygy <solson@bam.org>
2022-08-10 19:16:50 -0400

    Fix bug in send_email - should check both subject and body arguments

--- R/send_email.R
+++ R/send_email.R
@@ -11,7 +11,7 @@ send_email <- function(subject, body,
                        emails = config::get("tessiflow.email"),
                        smtp = config::get("tessiflow.smtp")) {
   assert_character(subject, len = 1)
-  assert_character(subject, len = 1)
+  assert_character(body, len = 1)
 
   if (!test_character(emails, min.len = 1)) {
     stop("Set tessiflow.email to the sender (first email) and list of recipients for error messages")
