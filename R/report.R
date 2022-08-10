6b389dba05006e2aa73782cb88524c63c244e309
skysyzygy <solson@bam.org>
2022-08-10 16:16:37 -0400

    Create flows_report to replace tasks.R

--- R/report.R
+++ R/report.R
@@ -1,1 +1,59 @@
 
+#' flows_report
+#' 
+#'
+#' @return nothing
+#' @importFrom lubridate now ddays as_datetime
+#' @importFrom dplyr tbl filter mutate transmute collect
+#' @describeIn flows_report Pull process data from the SQLite database from the last 24hours.
+flows_report_load <- function() {
+  
+  flows_log_open()
+  
+  tasks <- tbl(tessiflow$db,"jobs") %>% filter(start_time > !!as.integer(now() - ddays(1))) %>%
+    collect %>%
+    transmute(`Flow Name`=flow_name,
+              `Job Name`=job_name,
+              `Status` = ifelse(retval!=0 & status != "Running","Failed",status),
+              `Step`=step,
+              `Start Time`=as_datetime(start_time, tz=Sys.timezone()),
+              `End Time`=as_datetime(end_time, tz=Sys.timezone())) %>%
+    mutate(`Elapsed`=ifelse(is.na(`End Time`),
+                            format(interval_to_period(`Start Time`,now())),
+                            format(interval_to_period(`Start Time`,`End Time`))))
+  
+  if(nrow(tasks)==0)
+    return(invisible())
+  
+  return(tasks)
+  
+}
+
+#' @importFrom htmlTable addHtmlTableStyle htmlTable
+#' @importFrom dplyr case_when
+#' @describeIn flows_report Send the flows report
+flows_report_send <- function() {
+  
+   
+    subject <- "tessiflow report"
+    body <- flows_report_load() %>% 
+      addHtmlTableStyle(col.rgroup=case_when(.$Status=="Failed"~"#F77",
+                                             .$Status=="Running"~"#7F7",
+                                             TRUE~"none")) %>% 
+      htmlTable(rnames = FALSE)
+    
+    send_email(subject = subject,body=body)
+}
+
+#' interval_to_period
+#' 
+#' converts an interval of start and end times to a pretty printed elapsed time in the format
+#' `<hours> H <minutes> M <seconds> S`
+#'
+#' @param start_time datetime 
+#' @param end_time datetime
+#' @importFrom  lubridate as.period as.duration interval
+#' @return lubridate period object for pretty printing of time differences
+interval_to_period <- function(start_time,end_time) {
+  as.period(floor(as.duration(interval(start_time,end_time,tzone = Sys.timezone()))))
+}
