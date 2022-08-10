6a2aaa4b3417763256cdcf96274f5d525a442be8
skysyzygy <solson@bam.org>
2022-08-10 19:26:16 -0400

    Radically simplify tests -- no need to spawn extra processes each time, and using the callr package option allows us to attach to an existing namespace rather than loading at runtime.

--- tests/testthat/test-tessiflow.R
+++ tests/testthat/test-tessiflow.R
@@ -1,62 +1,58 @@
 withr::local_package("mockery")
 withr::local_package("devtools")
+local_log_dir()
 
 # tessiflow_run ---------------------------------------------------------
-run_expr <- quote({
-  tessiflow:::local_log_dir(envir = new.env())
-  tryCatch(devtools::load_all(quiet = TRUE),
-    error = function(e) {
-      library("tessiflow")
-    }
-  )
+
+run_fun <- function() {
+  local_log_dir(envir = new.env())
   mockery::stub(tessiflow_run, "flows_main", function() {
     message("Running flows_main()")
     Sys.sleep(10) # has to be long enough to allow the process to persist between tests
   })
   tessiflow_run()
-})
+}
 
 consume_output_lines <- function(process) {
+  output <- character()
   # consume the rest of the output lines
   process$poll_io(10000)
-  while (length(process$read_output_lines()) > 0) {
+  while (length(current_output<-process$read_output_lines()) > 0) {
+    append(output,current_output)
     Sys.sleep(1)
   }
+  output
 }
 
 num_processes <- 0
 
 test_that("tessiflow_run refuses to start if tessiflow is already running", {
   expect_equal(length(ps::ps_find_tree("tessiflow-daemon")), 0)
-
-  p1 <- callr::r_bg(eval, list(run_expr))
+  
+  p1 <- callr::r_bg(run_fun, package = "tessiflow")
   p1$poll_io(10000)
   p1_output <- p1$read_output_lines()
   expect_match(p1_output, "Starting tessiflow")
   consume_output_lines(p1)
+  expect_equal(p1$read_error_lines(),character())
   num_processes <<- length(ps::ps_find_tree("tessiflow-daemon"))
   expect_gte(num_processes, 1)
 
-  p2 <- callr::r_bg(eval, list(run_expr))
-  p2$poll_io(10000)
-  p2_error <- p2$read_error_lines()
-  expect_match(p2_error, "Found running tessiflow", all = FALSE)
+  expect_error(tessiflow_run(), "Found running tessiflow")
   expect_equal(length(ps::ps_find_tree("tessiflow-daemon")), num_processes)
 
   p1$kill_tree()
-  p2$kill_tree()
 })
 
 test_that("tessiflow_run logs to a log file", {
   expect_equal(length(ps::ps_find_tree("tessiflow-daemon")), 0)
+  stub(tessiflow_run, "flows_main", function() {
+    message("Running flows_main()")
+  })
+  
+  expect_output(tessiflow_run(),"Starting tessiflow",all = FALSE)
 
-  p1 <- callr::r_session$new(callr::r_session_options(stderr = "", stdout = ""))
-  logdir <- p1$run_with_output(eval, list(quote(config::get("tessiflow.log"))))$result
-  p1$call(eval, list(run_expr))
-  consume_output_lines(p1)
-
-  expect_length(logdir, 1) # message isn't printed to console
-  logdata <- readLines(file.path(logdir, "tessiflow.log"))
+  logdata <- readLines(file.path(config::get("tessiflow.log"), "tessiflow.log"))
   expect_match(logdata, "Starting tessiflow", all = FALSE)
   expect_match(logdata, "Running flows_main", all = FALSE)
   expect_equal(length(logdata), 2)
@@ -67,7 +63,7 @@ test_that("tessiflow_run logs to a log file", {
 # tessiflow_stop ----------------------------------------------------------
 
 test_that("tessiflow_stop kills the daemon process", {
-  p1 <- callr::r_bg(eval, list(run_expr))
+  p1 <- callr::r_bg(run_fun, package = "tessiflow")
   consume_output_lines(p1)
 
   expect_gte(length(ps::ps_find_tree("tessiflow-daemon")), 1)
@@ -76,20 +72,15 @@ test_that("tessiflow_stop kills the daemon process", {
 })
 
 test_that("tessiflow_stop kills all running jobs", {
-  run_expr <- quote({
-    tessiflow:::local_log_dir(envir = new.env())
-    tryCatch(devtools::load_all(quiet = TRUE),
-      error = function(e) {
-        library("tessiflow")
-      }
-    )
+  run_fun <- function() {
+    local_log_dir(envir = new.env())
     mockery::stub(tessiflow_run, "flows_main", function() {
       callr::r(Sys.sleep, list(10))
     })
     tessiflow_run()
-  })
+  }
 
-  p1 <- callr::r_bg(eval, list(run_expr))
+  p1 <- callr::r_bg(run_fun, package = "tessiflow")
   consume_output_lines(p1)
 
   expect_gte(length(ps::ps_find_tree("tessiflow-daemon")), num_processes)
@@ -150,27 +141,23 @@ test_that("tessiflow_run_command errors if there's no running tessiflow process"
 })
 
 test_that("tessiflow_run_command writes to the tessiflow input file/socket", {
-  run_expr <- quote({
-    tessiflow:::local_log_dir(envir = new.env())
-    tryCatch(devtools::load_all(quiet = TRUE),
-      error = function(e) {
-        library("tessiflow")
-      }
-    )
+  run_fun <- function() {
+    local_log_dir(envir = new.env())
     mockery::stub(flows_main, "flows_main_read_server", function(server) {
       socket <- try(socketAccept(server, timeout = 1), silent = TRUE)
       if (!"try-error" %in% class(socket)) {
         input <- readLines(socket, n = 1)
-        print(input)
+        if(length(input)>0)
+          print(input)
       }
     })
     flows_main()
-  })
+  }
 
   withr::local_envvar("tessiflow-daemon" = "YES")
   expect_equal(length(ps::ps_find_tree("tessiflow-daemon")), 0)
 
-  p1 <- callr::r_bg(eval, list(run_expr))
+  p1 <- callr::r_bg(run_fun, package = "tessiflow")
   consume_output_lines(p1)
 
   tessiflow_run_command("Dummy workflow", "Job 1", "this_is_a_function")
