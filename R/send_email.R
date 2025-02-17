ed14537d82a503de074d91bc89322c808ae711ec
Sky Syzygy <ssyzygy@bam.org>
2025-02-17 10:11:52 -0500

    Switch from mailR::send.mail to sendmailR::sendmail

--- R/send_email.R
+++ R/send_email.R
@@ -1,33 +1,45 @@
 #' send_email
 #'
-#' Wrapper for [`mailR::send.mail`] that does input checking and pulls in configuration data from the `config.yml` file
+#' Wrapper for [`sendmailR::sendmail`] that does input checking and pulls in configuration data from the `config.yml` file
 #'
 #' @param subject string subject line
 #' @param body string body of email
 #' @param emails list of email addresses, default is from `tessiflow.email` configuration variable; `from` address will be the first email
-#' @param smtp list of `SMTP` configuration data for [`mailR::send.mail`]
-#' @importFrom mailR send.mail
-send_email <- function(subject, body,
+#' @param smtp named list, should contain `hostname` and (optionally) `port`, which defaults to 25.
+#'  Additional elements will get merged with `...` and passed to curl, see [curl::curl_options] for more details.
+#' @importFrom sendmailR sendmail mime_part_html
+#' @importFrom checkmate assert assert_character check_character check_list test_character test_list
+#' @importFrom purrr keep_at
+send_email <- function(subject, body = paste("Sent by", Sys.info()["nodename"]),
                        emails = config::get("tessiflow.email"),
-                       smtp = config::get("tessiflow.smtp")) {
+                       smtp = config::get("tessiflow.smtp"),
+                       ...
+) {
   assert_character(subject, len = 1)
-  assert_character(body, len = 1)
-
+  assert(
+    check_character(body, len = 1),
+    check_list(body, "mime_part")
+  )
+  assert_character(attachments, null.ok = T)
+  
   if (!test_character(emails, min.len = 1)) {
-    stop("Set tessiflow.email to the sender (first email) and list of recipients for error messages")
+    stop("Set tessiflow.email to the sender (first email) and list of recipients for messages")
   }
   if (!test_list(smtp)) {
-    stop("Set tessiflow.smtp to the smtp server used to send error messages")
+    stop("Set tessiflow.smtp to a list containing the `hostname` and optionally `port` of the smtp server")
   }
-
-  send.mail(
+  
+  if (typeof(body) == "character")
+    body <- mime_part_html(body)
+  
+  dots <- modifyList(list(...),list(
     from = emails[[1]],
     to = emails,
-    subject = paste0(subject, " (", Sys.info()["nodename"], ")"),
-    body = body,
-    smtp = smtp,
-    encoding = "utf-8",
-    html = TRUE,
-    send = TRUE
-  )
+    subject = subject,
+    msg = body,
+    control = list(smtpServer=smtp$hostname,smtpPort=smtp$port),
+    engine = engine,
+    engineopts = keep_at(smtp,names(curl::curl_options()))))
+  
+  do.call(sendmail,dots)
 }
